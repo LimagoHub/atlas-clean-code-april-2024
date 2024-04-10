1fd6fe79786c8e0cd39988dca1f8377175f1b067
Jo Wagner <jowagner@limago.de>
2024-04-10 15:37:43 +0200

    bootstrap solution

--- VectorFillerUebung/bootstrap/Bootstrap.h
+++ VectorFillerUebung/bootstrap/Bootstrap.h
@@ -4,12 +4,49 @@
 
 #pragma once
 #include <iostream>
+#include <memory>
+#include <thread>
+#include "../client/impl/ClientImpl.h";
+#include "../generator/impl/random/MersenneTwisterNumberGenerator.h"
+#include "../collection/impl/sequential/VectorFactorySequentialImpl.h"
+#include "../collection/impl/decorators//VectorFactoryBenchmarkDecorator.h"
+
 namespace atlas::bootstrap {
     class Bootstrap {
+        using VECTOR_FACTORY = std::unique_ptr<atlas::collection::VectorFactory<int>>;
+        using VECTOR_FACTORY_SEQUENCIAL = atlas::collection::VectorFactorySequentialImpl<int>;
+        using VECTOR_FACTORY_BENCHMARK = atlas::collection::VectorFactoryBenchmarkDecorator<int>;
+        using GENERATOR = std::unique_ptr<generator::Generator<int>>;
+        using CLIENT = std::unique_ptr<atlas::client::Client>;
+
+
+        static GENERATOR createGenerator() {
+            GENERATOR generator = std::make_unique<atlas::generator::MersenneTwisterNumberGenerator>();
+            return generator;
+        }
+
+        static VECTOR_FACTORY createVectorFactory(GENERATOR generator) {
+            VECTOR_FACTORY result;
+            result = std::make_unique<VECTOR_FACTORY_SEQUENCIAL>(std::move(generator));
+            result = std::make_unique<VECTOR_FACTORY_BENCHMARK>(std::move(result));
+            return result;
+        }
+
+        static CLIENT createClient(VECTOR_FACTORY factory) {
+            CLIENT client = std::make_unique<atlas::client::VectorClientImpl>(std::move(factory));
+            return client;
+        }
+
+
 
     public:
         void startApplication() {
-            std::cout << "App startet" << std::endl;
+
+            auto generator = createGenerator();
+            auto vectorFiller = createVectorFactory(std::move(generator));
+            auto client = createClient(std::move(vectorFiller));
+
+            client->doSomethingWithLargeVector();
         }
 
     };
